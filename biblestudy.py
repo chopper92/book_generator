4e60a13d867be3e961ae45675051805d3293f1d1
Claire Hopper <43118437+chopper92@users.noreply.github.com>
2022-10-11 21:56:32 -0400

    Update biblestudy.py

--- biblestudy.py
+++ biblestudy.py
@@ -4,7 +4,7 @@ import random
 
 old_testament = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachai']
 new_testament = ['Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation' ]
-
+chapters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
 #Selecting a random book in the Old Testament for today's study.
 print('Which book in the Old Testament would you like to read? ') 
@@ -14,6 +14,9 @@ print(random.choice(old_testament))
 print('Which book in the New Testament would you like to read? ')
 print(random.choice(new_testament))
 
+print('Which chapter would you like to read? ')    
+print(random.choice(chapters)) 
+    
     
 wait = input("PRESS ENTER TO END.")
 
